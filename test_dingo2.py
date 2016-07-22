a03efd8aeac32b05332285aed328884fe5d0b2f4
Jonathan Amme <jonathan.amme@rl-institut.de>
2016-07-22 15:52:20 +0200

    do grid parametrization prior to routing

--- test_dingo2.py
+++ test_dingo2.py
@@ -22,16 +22,16 @@ nd = NetworkDingo(name='network')
 # get database connection info from config file
 conn = db.connection(section='oedb')
 
-# mv_grid_districts=[360, 571, 593, 368, 491, 425, 416, 372, 387, 407, 403, 373, 482] # some MV grid_districts from SPF region
+#mv_grid_districts=[360, 571, 593, 368, 491, 425, 416, 372, 387, 407, 403, 373, 482] # some MV grid_districts from SPF region
 mv_grid_districts=[482]
 
 nd.import_mv_grid_districts(conn, mv_grid_districts)
 
 conn.close()
 
-nd.mv_routing(debug=False, animation=False)
-
 nd.mv_parametrize_grid()
+
+nd.mv_routing(debug=False, animation=False)
 
 conn = db.connection(section='oedb')
 nd.export_mv_grid(conn, mv_grid_districts)
