9e017a8d3d08079d286f817d182b648033011251
nesnoj <jonathan.amme@rl-institut.de>
2016-08-03 10:07:32 +0200

    modify db table def of vizualization
    Add LV stations

--- dingo/config/config_db_interfaces.py
+++ dingo/config/config_db_interfaces.py
@@ -17,7 +17,8 @@ class sqla_mv_grid_viz(Base):
 
     grid_id                     = sa.Column('grid_id', sa.Integer(), primary_key=True)
     geom_mv_station             = sa.Column('geom_mv_station', Geometry(geometry_type='POINT', srid=4326))
-    geom_mv_cable_dist          = sa.Column('geom_mv_cable_dist', Geometry(geometry_type='MULTIPOINT', srid=4326))
+    geom_mv_cable_dists         = sa.Column('geom_mv_cable_dists', Geometry(geometry_type='MULTIPOINT', srid=4326))
     geom_mv_circuit_breakers    = sa.Column('geom_mv_circuit_breakers', Geometry(geometry_type='MULTIPOINT', srid=4326))
+    geom_lv_load_area_centres   = sa.Column('geom_lv_load_area_centres', Geometry(geometry_type='MULTIPOINT', srid=4326))
     geom_lv_stations            = sa.Column('geom_lv_stations', Geometry(geometry_type='MULTIPOINT', srid=4326))
     geom_mv_lines               = sa.Column('geom_mv_lines', Geometry(geometry_type='MULTILINESTRING', srid=4326))
