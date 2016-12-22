dabaef1eab40a62b03cc2e30e75049e7d632baf4
nesnoj <jonathan.amme@rl-institut.de>
2016-12-22 14:01:58 +0100

    move test_dingo2.py to examples dir and rename it

--- examples/example.py
+++ examples/example.py
@@ -24,7 +24,7 @@ nd = NetworkDingo(name='network')
 # mv_grid_districts=[360, 571, 593, 368, 491, 416, 372, 387, 407, 403, 373, 482] # some MV grid_districts from SPF region
 # mv_grid_districts=[482]
 # mv_grid_districts = [386,372,406,371,402,415,480,424,489,367,359,569,591]
-mv_grid_districts=[489]
+mv_grid_districts=[480]
 
 nd.import_mv_grid_districts(conn, mv_grid_districts)
 nd.import_generators(conn)
