b3ebb5d35cdb990eab59b60a30867d82a0f87387
nesnoj <jonathan.amme@rl-institut.de>
2016-12-23 12:28:46 +0100

    add missing meta data and include package data in setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,18 @@
 from setuptools import find_packages, setup
+import os
+import dingo
+
 # import subprocess
 #
 # subprocess.call(["sudo", "apt-get", "install", "libgeos-dev"])
 
 setup(name='dingo',
-      author='openego development group',
+      version='v0.1-pre',
+      author='Reiner Lemoine Institut, openego development group',
+      author_email='jonathan.amme@rl-institut.de',
       description='DIstribution Network GeneratOr',
+      url='https://github.com/openego/dingo',
+      license='GNU GPLv3',
       packages=find_packages(),
       install_requires=['networkx >= 1.11, <= 1.11',
                         'geopy >= 1.11.0, <= 1.11.0',
@@ -20,6 +27,46 @@ setup(name='dingo',
                         'egopowerflow >= 0.0.2, <= 0.0.2',
                         'shapely >= 1.5.12, <= 1.5.12',
                         'pypsa >= 0.7.1, <= 0.7.1'
-                        ]
-     )
+                        ],
+      package_data={
+          'config': [
+              os.path.join('config',
+                           '*.cfg'),
+            ],
+          'data': [
+              os.path.join('data',
+                           '*.csv'),
+            ],
+          'testcases': [
+              os.path.join('grid',
+                           'mv_grid',
+                           'tests',
+                           'testcases',
+                           '*.vrp'),
+              os.path.join('grid',
+                           'mv_grid',
+                           'tests',
+                           'testcases',
+                           'Augerat',
+                           '*.vrp'),
+              os.path.join('grid',
+                           'mv_grid',
+                           'tests',
+                           'testcases',
+                           'Augerat-tcc',
+                           '*.vrp'),
+              os.path.join('grid',
+                           'mv_grid',
+                           'tests',
+                           'testcases',
+                           'Takes-tcc',
+                           '*.vrp'),
+              os.path.join('grid',
+                           'mv_grid',
+                           'tests',
+                           'testcases',
+                           'Vigo',
+                           '*.vrp'),
 
+          ]}
+      )
