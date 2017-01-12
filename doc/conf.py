da598af85a6384a5b2dd7d2b69b02d41ccae371f
nesnoj <jonathan.amme@rl-institut.de>
2017-01-12 15:05:44 +0100

    fix import errors on libraries that depend on C modules

--- doc/conf.py
+++ doc/conf.py
@@ -118,7 +118,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
             return MagicMock()
 
-MOCK_MODULES = ['geos_c']
+MOCK_MODULES = ['libgeos', 'geos', 'libgeos_c']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 
