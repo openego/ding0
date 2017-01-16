1c9f0617d73845fe8f111e34755001138b5cc4b9
nesnoj <jonathan.amme@rl-institut.de>
2017-01-16 17:07:12 +0100

    add sqla bus and branch obj to db interfaces

--- dingo/config/config_db_interfaces.py
+++ dingo/config/config_db_interfaces.py
@@ -23,3 +23,38 @@ class sqla_mv_grid_viz(Base):
     geom_lv_stations            = sa.Column('geom_lv_stations', Geometry(geometry_type='MULTIPOINT', srid=4326))
     geom_mv_generators          = sa.Column('geom_mv_generators', Geometry(geometry_type='MULTIPOINT', srid=4326))
     geom_mv_lines               = sa.Column('geom_mv_lines', Geometry(geometry_type='MULTILINESTRING', srid=4326))
+
+
+class sqla_mv_grid_viz_branches(Base):
+    """ SQLAlchemy table definition for the export of MV grids' branches for visualization purposes
+    """
+
+    __tablename__ = 'ego_deu_mv_grids_vis_branches'
+    __table_args__ = {'schema': 'calc_ego_grid'}
+
+    branch_id                   = sa.Column(sa.String(25), primary_key=True)
+    grid_id                     = sa.Column('grid_id', sa.Integer)
+    type_name                   = sa.Column('type_name', sa.String(25))
+    type_kind                   = sa.Column('type_kind', sa.String(5))
+    type_v_nom                  = sa.Column('type_v_nom', sa.Integer)
+    type_s_nom                  = sa.Column('type_s_nom', sa.Float(53))
+    length                      = sa.Column('length', sa.Float(53))
+    geom                        = sa.Column('geom', Geometry(geometry_type='LINESTRING', srid=4326))
+    s_res0                      = sa.Column('s_res0', sa.Float(53))
+    s_res1                      = sa.Column('s_res1', sa.Float(53))
+
+
+class sqla_mv_grid_viz_buses(Base):
+    """ SQLAlchemy table definition for the export of MV grids' branches for visualization purposes
+    """
+
+    __tablename__ = 'ego_deu_mv_grids_vis_buses'
+    __table_args__ = {'schema': 'calc_ego_grid'}
+
+
+    bus_id                      = sa.Column(sa.String(25), primary_key=True)
+    grid_id                     = sa.Column('grid_id', sa.Integer)
+    type_v_nom                  = sa.Column('type_v_nom', sa.Integer)
+    geom                        = sa.Column('geom', Geometry(geometry_type='POINT', srid=4326))
+    v_res0                      = sa.Column('v_res0', sa.Float(53))
+    v_res1                      = sa.Column('v_res1', sa.Float(53))
