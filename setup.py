b7bdf28bae4af84b0bfd6fdcf628e462098b4a0e
nesnoj <jonathan.amme@rl-institut.de>
2017-01-27 16:11:11 +0100

    change version reqs in setup.py

--- setup.py
+++ setup.py
@@ -16,8 +16,8 @@ setup(name='dingo',
       packages=find_packages(),
       install_requires=['networkx >= 1.11, <= 1.11',
                         'geopy >= 1.11.0, <= 1.11.0',
-                        'pandas >= 0.17.0, <= 0.19.1',
-                        'pyomo >= 5.0.1, <= 5.0.1',
+                        'pandas >= 0.17.0, <= 0.19.2',
+                        'pyomo >= 5.0.1, <= 5.1.1',
                         'pyproj >= 1.9.5.1, <= 1.9.5.1',
                         'sqlalchemy >= 1.0.11, <= 1.1.4',
                         'geoalchemy2 >= 0.2.6, <= 0.4.0',
@@ -26,7 +26,7 @@ setup(name='dingo',
                         'oemof.db  >= 0.0.4, <= 0.0.4',
                         'egopowerflow >= 0.0.2, <= 0.0.2',
                         'shapely >= 1.5.12, <= 1.5.12',
-                        'pypsa >= 0.7.1, <= 0.7.1'
+                        'pypsa >= 0.7.1, <= 0.8.0'
                         ],
       package_data={
           'config': [
