6b63737fdfa501825de04e85894e70544f04be3c
gplessm <guido.plessmann@rl-institut.de>
2017-03-27 17:36:47 +0200

    Rework results

--- dingo/tools/results.py
+++ dingo/tools/results.py
@@ -15,196 +15,208 @@ class ResultsDingo:
 
     """
 
-    def __init__(self, mv_grid_districts=None, filenames=None, base_path=''):
+    def __init__(self, base_path):
         """
-
-        Either provide list of mv_grid_districts or dict of file names
-
-        filenames : dict
-            File names. Dictionary with certain keys
-                * nd
-                * edges
-                * nodes
-
-        `base_path` is optional, but it's actually required
 
         Parameters
         ----------
+       `base_path` : str
+            Path to results dir structure
         """
 
-        if mv_grid_districts is not None:
-            self.mv_grid_districts_including_invalid = mv_grid_districts
-        else:
-            self.mv_grid_districts_including_invalid = None
+        # if mv_grid_districts is not None:
+        #     self.mv_grid_districts_including_invalid = mv_grid_districts
+        # else:
+        #     self.mv_grid_districts_including_invalid = None
 
 
         self.base_path = base_path
 
-        self.nd = None
         self.edges = None
         self.nodes = None
         self.global_stats = None
         self.mvgd_stats = None
 
-        if os.path.isfile(os.path.join(self.base_path,
-                         'info',
-                          'corrupt_mv_grid_districts_{0}-{1}.txt'.format(
-                              mv_grid_districts[0], mv_grid_districts[-1]))):
-            self.invalid_mvgd = pd.read_csv(
-                os.path.join(self.base_path,
-                             'info',
-                             'corrupt_mv_grid_districts_{0}-{1}.txt'.format(
-                              mv_grid_districts[0], mv_grid_districts[-1])))
+        # if os.path.isfile(os.path.join(self.base_path,
+        #                  'info',
+        #                   'corrupt_mv_grid_districts_{0}-{1}.txt'.format(
+        #                       mv_grid_districts[0], mv_grid_districts[-1]))):
+        #     self.invalid_mvgd = pd.read_csv(
+        #         os.path.join(self.base_path,
+        #                      'info',
+        #                      'corrupt_mv_grid_districts_{0}-{1}.txt'.format(
+        #                       mv_grid_districts[0], mv_grid_districts[-1])))
 
-        # get list of excluded grids (invalid) from info file
-        self.excluded_grid_districts = self.invalid_mvgd['id'].tolist()
+        # # get list of excluded grids (invalid) from info file
+        # self.excluded_grid_districts = self.invalid_mvgd['id'].tolist()
 
-        # define currently valid mv_grid districts
-        if self.mv_grid_districts_including_invalid is not None:
-            self.mv_grid_districs = [
-                mv for mv in self.mv_grid_districts_including_invalid
-                if mv not in self.excluded_grid_districts]
+        # # define currently valid mv_grid districts
+        # if self.mv_grid_districts_including_invalid is not None:
+        #     self.mv_grid_districs = [
+        #         mv for mv in self.mv_grid_districts_including_invalid
+        #         if mv not in self.excluded_grid_districts]
 
         # read results data from a single file
-        if filenames is not None:
-            if 'nd' in list(filenames.keys()):
-                self.nd = self.read_nd_multiple_mvgds(filenames['nd'])
-            if 'edges' in list(filenames.keys()):
-                self.edges = pd.read_csv(os.path.join(self.base_path,
-                                                      'results',
-                                                      filenames['edges']))
-            if 'nodes' in list(filenames.keys()):
-                self.nodes = pd.read_csv(os.path.join(self.base_path,
-                                                      'results',
-                                                      filenames['nodes']))
+        # if filenames is not None:
+        #     if 'nd' in list(filenames.keys()):
+        #         self.nd = self.read_nd_multiple_mvgds(filenames['nd'])
+        #     if 'edges' in list(filenames.keys()):
+        #         self.edges = pd.read_csv(os.path.join(self.base_path,
+        #                                               'results',
+        #                                               filenames['edges']))
+        #     if 'nodes' in list(filenames.keys()):
+        #         self.nodes = pd.read_csv(os.path.join(self.base_path,
+        #                                               'results',
+        #                                               filenames['nodes']))
         # read results from single file per mv grid district
-        elif mv_grid_districts is not None:
-            self.collect_data_from_file()
+        # elif mv_grid_districts is not None:
+        #     self.collect_data_from_file()
 
         # if mv grid district list is still unknown, get from results data
-        if (self.excluded_grid_districts is not None and
-            self.nd is not None
-            and mv_grid_districts is None):
-            self.mv_grid_districs = [
-                int(self.nd._mv_grid_districts[id].id_db)
-                for id in list(range(0, self.nd._mv_grid_districts.__len__()))]
-            self.mv_grid_districts_including_invalid = self.mv_grid_districs + \
-                self.excluded_grid_districts
+        # if (self.excluded_grid_districts is not None and
+        #     self.nd is not None
+        #     and mv_grid_districts is None):
+        #     self.mv_grid_districs = [
+        #         int(self.nd._mv_grid_districts[id].id_db)
+        #         for id in list(range(0, self.nd._mv_grid_districts.__len__()))]
+        #     self.mv_grid_districts_including_invalid = self.mv_grid_districs + \
+        #         self.excluded_grid_districts
 
-    def read_pickles_from_files(self, pickle_name):
+    def concat_nd_pickles(self, mv_grid_districts):
         """
-        Read multiple pickles and join nd objects
+        Read multiple pickles, join nd objects and save to file
 
-        :param pickle_name:
-        :return:
+        Parameters
+        ----------
+        mv_grid_districts : list
+            Ints describing MV grid districts
         """
+
+        pickle_name = cfg_dingo.get('output', 'nd_pickle')
+        # self.nd = self.read_pickles_from_files(pickle_name)
+
         mvgd_1 = pickle.load(
             open(os.path.join(
                 self.base_path,
                 'results',
-                pickle_name.format(self.mv_grid_districs[0])),
+                pickle_name.format(mv_grid_districts[0])),
                 'rb'))
 
-        for mvgd in self.mv_grid_districs[1:]:
-            mvgd_pickle = pickle.load(open(os.path.join(
+        for mvgd in mv_grid_districts[1:]:
+
+            filename = os.path.join(
                 self.base_path,
-                'results', pickle_name.format(mvgd)), 'rb'))
-            mvgd_1.add_mv_grid_district(mvgd_pickle._mv_grid_districts[0])
+                'results', pickle_name.format(mvgd))
+            if os.path.isfile(filename):
+                mvgd_pickle = pickle.load(open(filename, 'rb'))
+                if mvgd_pickle._mv_grid_districts:
+                    mvgd_1.add_mv_grid_district(mvgd_pickle._mv_grid_districts[0])
 
-        return mvgd_1
+        # save to concatenated pickle
+        pickle.dump(mvgd_1,
+                    open(os.path.join(
+                        self.base_path,
+                        'results',
+                        "dingo_grids_{0}-{1}.pkl".format(
+                            mv_grid_districts[0],
+                            mv_grid_districts[-1])),
+                        "wb"))
 
-    def read_csv_from_files(self, csv_name):
-        """
-        Read multiple CSV files and concatenate these
-        :param csv_name:
-        :return:
-        """
-        mvgd_1 = pd.read_csv(
-            os.path.join(
-                self.base_path,
-                'results',
-                csv_name.format(self.mv_grid_districs[0])))
-
-        for mvgd in self.mv_grid_districs[1:]:
-            mvgd_df = pd.read_csv(os.path.join(
-                self.base_path,
-                'results', csv_name.format(mvgd)))
-            mvgd_1 = mvgd_1.append(mvgd_df, ignore_index=True)
-
-        return mvgd_1
-
-    def collect_data_from_file(self):
-        """
-        Read results data from multiple files
-
-        :return:
-        """
-
-        # load nd object from pickle
-        pickle_name = cfg_dingo.get('output', 'nd_pickle')
-        self.nd = self.read_pickles_from_files(pickle_name)
-
-        # # load nodes and edges table data
-        # edges = cfg_dingo.get('output', 'edges_stats')
-        # self.edges = self.read_csv_from_files(edges)
-        # self.edges['grid_id'] = self.edges['grid_id'].map(
-        #     lambda x: '%.0f' % x)
-        # nodes = cfg_dingo.get('output', 'nodes_stats')
-        # self.nodes = self.read_csv_from_files(nodes)
-        # self.nodes['grid_id'] = self.nodes['grid_id'].map(
-        #     lambda x: '%.0f' % x)
-
-    def read_nd_multiple_mvgds(self, filename):
-        """
-        Reads file of multiple grid district stored in base path
-
-        Parameters
-        ----------
-        filename : str
-            Filename.
-            File must be saved in `base_path` structure.
-        """
-        data = pickle.load(
-            open(os.path.join(
-                self.base_path,
-                'results',
-                filename),
-                'rb'))
-        return data
-
-    def save_merge_data(self):
-        """Save complete dataset of a run"""
-        pickle.dump(self.nd,
-                open(os.path.join(
-                    self.base_path,
-                    'results',
-                    "dingo_grids_{0}-{1}.pkl".format(
-                    self.mv_grid_districs[0], self.mv_grid_districs[-1])),
-                     "wb"))
-        # self.nodes.to_csv(os.path.join(
-        #             self.base_path,
-        #             'results', 'mvgd_nodes_stats_{0}-{1}.csv'.format(
-        #             self.mv_grid_districs[0], self.mv_grid_districs[-1])),
-        #     index=False
-        # )
-        # self.edges.to_csv(os.path.join(
-        #             self.base_path,
-        #             'results', 'mvgd_edges_stats_{0}-{1}.csv'.format(
-        #             self.mv_grid_districs[0], self.mv_grid_districs[-1])),
-        #     index=False
-        # )
-
-        nodes, edges = self.nd.to_dataframe()
+        # save stats (edges and nodes data) to csv
+        nodes, edges = mvgd_1.to_dataframe()
         nodes.to_csv(os.path.join(
             self.base_path,
             'results', 'mvgd_nodes_stats_{0}-{1}.csv'.format(
-                self.mv_grid_districs[0], self.mv_grid_districs[-1])),
+                mv_grid_districts[0], mv_grid_districts[-1])),
             index=False)
         edges.to_csv(os.path.join(
             self.base_path,
             'results', 'mvgd_edges_stats_{0}-{1}.csv'.format(
-                self.mv_grid_districs[0], self.mv_grid_districs[-1])),
+                mv_grid_districts[0], mv_grid_districts[-1])),
             index=False)
+
+
+    def concat_csv_stats_files(self, ranges):
+        """
+        Concatenate multiple csv files containing statistics on nodes and edges.
+
+
+        Parameters
+        ----------
+        ranges : list
+            The list contains tuples of 2 elements describing start and end of
+            each range.
+        """
+
+        for f in ['nodes', 'edges']:
+            file_base_name = 'mvgd_' + f + '_stats_{0}-{1}.csv'
+
+            filenames = []
+            [filenames.append(file_base_name.format(mvgd_ids[0], mvgd_ids[1]))
+             for mvgd_ids in ranges]
+
+            results_file = 'mvgd_{0}_stats_{1}-{2}.csv'.format(
+                f, ranges[0][0], ranges[-1][-1])
+
+            self.concat_and_save_csv(filenames, results_file)
+
+
+    def concat_and_save_csv(self, filenames, result_filename):
+        """
+        Concatenate and save multiple csv files in `base_path` specified by
+        filnames
+
+        The path specification of files in done via `self.base_path` in the
+        `__init__` method of this class.
+
+
+        Parameters
+        filenames : list
+            Files to be concatenates
+        result_filename : str
+            File name of resulting file
+
+        """
+
+        list_ = []
+
+        for filename in filenames:
+            df = pd.read_csv(os.path.join(self.base_path, 'results', filename),
+                             index_col=None, header=0)
+            list_.append(df)
+
+        frame = pd.concat(list_)
+        frame.to_csv(os.path.join(
+            self.base_path,
+            'results', result_filename), index=False)
+
+    def read_csv_results(self, concat_csv_file_range):
+        """
+        Read csv files (nodes and edges) containing results figures
+        Parameters
+        ----------
+        concat_csv_file_range : list
+            Ints describe first and last mv grid id
+        """
+
+        self.nodes = pd.read_csv(
+            os.path.join(self.base_path,
+                         'results',
+                         'mvgd_nodes_stats_{0}-{1}.csv'.format(
+                             concat_csv_file_range[0], concat_csv_file_range[-1]
+                         ))
+        )
+
+        self.edges = pd.read_csv(
+            os.path.join(self.base_path,
+                         'results',
+                         'mvgd_edges_stats_{0}-{1}.csv'.format(
+                             concat_csv_file_range[0], concat_csv_file_range[-1]
+                         ))
+        )
+
+
+
 
     def global_stats(self):
 
--- dingo/tools/results_scigrid_2017.py
+++ dingo/tools/results_scigrid_2017.py
@@ -7,26 +7,25 @@ cfg_dingo.load_config('config_files.cfg')
 cfg_dingo.load_config('config_misc.cfg')
 
 base_path = "/home/guido/rli_local/dingo_results/"
+dingo = results.ResultsDingo(base_path)
 
-first_mvgd = 5
-last_mvgd = 24
+# concat nd-pickles
+# first_mvgd = 5
+# last_mvgd = 24
+# dingo.concat_nd_pickles(list(range(first_mvgd, last_mvgd + 1)))
 
-# read data from multiple files a merge to one
-# mv_grid_districts = list(range(first_mvgd,last_mvgd + 1))
-# dingo_results = results.ResultsDingo(mv_grid_districts=mv_grid_districts,
-#                                      base_path=base_path)
-# dingo_results.save_merge_data()
+# concat csv files of larger ranges
+# ranges = [tuple([5,15]), tuple([16,24])]
+# dingo.concat_csv_stats_files(ranges)
 
-# evaluate dingo grids
-dingo_results = results.ResultsDingo(
-    filenames={
-        'nd': 'dingo_grids_{0}-{1}.pkl'.format(first_mvgd, last_mvgd),
-        'nodes': 'mvgd_nodes_stats_{0}-{1}.csv'.format(first_mvgd, last_mvgd),
-        'edges': 'mvgd_edges_stats_{0}-{1}.csv'.format(first_mvgd, last_mvgd)
-    },
-    base_path=base_path)
-global_stats = dingo_results.calculate_global_stats()
-mvgd_stats = dingo_results.calculate_mvgd_stats()
-dingo_results.plot_cable_length()
-for key in list(global_stats.keys()):
-    print(key, global_stats[key])
+# create results figures and numbers based of concatenated csv file
+concat_csv_file_range = [5, 24]
+dingo.read_csv_results(concat_csv_file_range)
+
+# calculate stats
+# global_stats = dingo.calculate_global_stats()
+mvgd_stats = dingo.calculate_mvgd_stats()
+print(mvgd_stats)
+dingo.plot_cable_length()
+# for key in list(global_stats.keys()):
+#     print(key, global_stats[key])
