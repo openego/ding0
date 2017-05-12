1a20b05116aa644c26902db0794c17361176d575
nesnoj <jonathan.amme@rl-institut.de>
2017-05-12 16:57:49 +0200

    first version of SD relocation

--- dingo/grid/mv_grid/tools.py
+++ dingo/grid/mv_grid/tools.py
@@ -1,5 +1,5 @@
 from dingo.core.network.stations import LVStationDingo
-from dingo.core.network import CableDistributorDingo
+from dingo.core.network import CableDistributorDingo, GeneratorDingo
 from dingo.tools.geo import calc_geo_centre_point
 import logging
 
@@ -37,49 +37,67 @@ def set_circuit_breakers(mv_grid, debug=False):
     """
     # TODO: add references (Tao)
 
-    # set init value
-    demand_diff_min = 10e6
-
     # iterate over all rings and circuit breakers
     for ring, circ_breaker in zip(mv_grid.rings_nodes(include_root_node=False), mv_grid.circuit_breakers()):
 
         nodes_peak_load = []
+        nodes_peak_generation = []
+
         # iterate over all nodes of ring
         for node in ring:
 
-            # node is LV station -> get peak load
+            # node is LV station -> get peak load and peak generation
             if isinstance(node, LVStationDingo):
-                nodes_peak_load.append(node.peak_load)
+                #nodes_peak_load.append(node.peak_load)
+                #nodes_peak_generation.append(node.peak_generation)
+                nodes_peak_load.append(node.peak_load - node.peak_generation)
 
             # node is cable distributor -> get all connected nodes of subtree using graph_nodes_from_subtree()
             elif isinstance(node, CableDistributorDingo):
                 nodes_subtree = mv_grid.graph_nodes_from_subtree(node)
                 nodes_subtree_peak_load = 0
+                nodes_subtree_peak_generation = 0
+
                 for node_subtree in nodes_subtree:
 
-                    # node is LV station -> get peak load
+                    # node is LV station -> get peak load and peak generation
                     if isinstance(node_subtree, LVStationDingo):
                         nodes_subtree_peak_load += node_subtree.peak_load
+                        nodes_subtree_peak_generation += node_subtree.peak_generation
 
-                nodes_peak_load.append(nodes_subtree_peak_load)
+                    # node is LV station -> get peak load and peak generation
+                    if isinstance(node_subtree, GeneratorDingo):
+                        nodes_subtree_peak_generation += node_subtree.capacity
+
+                #nodes_peak_load.append(nodes_subtree_peak_load)
+                #nodes_peak_generation.append(nodes_subtree_peak_generation)
+                nodes_peak_load.append(nodes_subtree_peak_load - nodes_subtree_peak_generation)
 
             else:
                 raise ValueError('Ring node has got invalid type.')
 
+        # is ring dominated by load or generation?
+        # (check if there's more load than generation in ring or vice versa)
+        #if sum(nodes_peak_load) > sum(nodes_peak_generation):
+        node_peak_data = nodes_peak_load
+        #else:
+        #    node_peak_data = nodes_peak_generation
+
         # calc optimal circuit breaker position
 
         # set init value
-        demand_diff_min = 10e6
+        diff_min = 10e6
 
-        # check possible positions in route
-        for ctr in range(len(nodes_peak_load)):
+        # check where difference of demand/generation in two half-rings is minimal
+        for ctr in range(len(node_peak_data)):
             # split route and calc demand difference
-            route_demand_part1 = sum(nodes_peak_load[0:ctr])
-            route_demand_part2 = sum(nodes_peak_load[ctr:len(nodes_peak_load)])
-            demand_diff = abs(route_demand_part1 - route_demand_part2)
+            route_data_part1 = sum(node_peak_data[0:ctr])
+            route_data_part2 = sum(node_peak_data[ctr:len(node_peak_data)])
+            diff = abs(route_data_part1 - route_data_part2)
 
-            if demand_diff <= demand_diff_min:  # equality has to be respected, otherwise comparison stops when demand=0
-                demand_diff_min = demand_diff
+            # equality has to be respected, otherwise comparison stops when demand/generation=0
+            if diff <= diff_min:
+                diff_min = diff
                 position = ctr
             else:
                 break
