93990493197d1eeb6f457c17ee869b6888f3b055
gplessm <guido.plessmann@rl-institut.de>
2017-05-30 16:12:00 +0200

    Update Dingo requirements

--- dingo_system_dependencies.sh
+++ dingo_system_dependencies.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 
-sudo apt-get install python3-dev liblapack-dev libatlas-base-dev libfreetype6-dev pkg-config libgeos-dev libpq-dev libffi-dev
+# update package index
+apt-get update -y
+
+# non-required dependencies
+#apt-get install -y liblapack-dev
+# apt-get install -y libatlas-base-dev
+# apt-get install -y libfreetype6-dev
+# apt-get install -y pkg-config
+# apt-get install -y libpq-dev
+# apt-get install -y build-essential
+# apt-get install -y g++
+
+# definitely needed packages
+apt-get install -y libssl-dev
+apt-get install -y python3-dev
+apt-get install -y libgeos-dev
+apt-get install -y libffi-dev
+apt-get install -y gcc
