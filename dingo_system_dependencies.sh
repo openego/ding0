83391c270cf5275fcea350ae9661c978517578fd
gplessm <guido.plessmann@rl-institut.de>
2017-06-01 16:44:44 +0200

    Update system package requirements list

--- dingo_system_dependencies.sh
+++ dingo_system_dependencies.sh
@@ -3,18 +3,10 @@
 # update package index
 apt-get update -y
 
-# non-required dependencies
-#apt-get install -y liblapack-dev
-# apt-get install -y libatlas-base-dev
-# apt-get install -y libfreetype6-dev
-# apt-get install -y pkg-config
-# apt-get install -y libpq-dev
-# apt-get install -y build-essential
-# apt-get install -y g++
-
-# definitely needed packages
+# required packages
 apt-get install -y libssl-dev
 apt-get install -y python3-dev
 apt-get install -y libgeos-dev
 apt-get install -y libffi-dev
 apt-get install -y gcc
+apt-get install -y python3-tk
