9248cdf734a5b6f5603412b001a5dd9ea9729e61
Miguel Parada <RL-INSTITUT\miguel.parada@solydxk>
2017-06-22 11:49:58 +0200

    Regression Tests Working fine

--- dingo/reg-tests/test_dingoruns.py
+++ dingo/reg-tests/test_dingoruns.py
@@ -3,15 +3,14 @@ from test import support
 
 # import DB interface from oemof
 import oemof.db as db
-
 # import other dingo stuff
 from dingo.core import NetworkDingo
 from dingo.tools import results
 from dingo.tools.logger import setup_logger
 from dingo.tools.results import save_nd_to_pickle
 
-#from numpy import array_equal
-import numpy as np
+from geoalchemy2.shape import to_shape
+
 
 logger = setup_logger()
 
@@ -19,119 +18,79 @@ class DingoRunTest(unittest.TestCase):
 
     def setUp(self):
         print("setup")
-        filename = 'dingo_grids_example.pkl'
-        mv_grid_districts = [3545]
-        # database connection
-        self.conn = db.connection(section='oedb')
-
-        # load data from file
-        self.nd_sav = results.load_nd_from_pickle(filename=filename)
-
-
-        # Fresh run from dingo
-        # instantiate new dingo network object
-        #self.nd_sav = NetworkDingo(name='network')
-
-        # run DINGO on selected MV Grid District
-        #self.nd_sav.run_dingo(conn=self.conn,
-        #                      mv_grid_districts_no=mv_grid_districts)
-
-        #self.nd_sav.control_circuit_breakers(mode='close')
-        #self.nd_sav.export_mv_grid(self.conn, mv_grid_districts)
-        #self.nd_sav.export_mv_grid_new(self.conn, mv_grid_districts)
-
-
-        # New run from dingo
-        # instantiate new dingo network object
-        #self.nd_new = NetworkDingo(name='network')
-
-        # run DINGO on selected MV Grid District
-        #self.nd_new.run_dingo(conn=self.conn,
-        #                      mv_grid_districts_no=mv_grid_districts)
-
-        #self.nd_new.control_circuit_breakers(mode='close')
-        #self.nd_new.export_mv_grid(self.conn, mv_grid_districts)
-        #self.nd_new.export_mv_grid_new(self.conn, mv_grid_districts)
-
-        self.nd_new = results.load_nd_from_pickle(filename=filename)
-
-        #self.nd_new = self.nd_sav
+        self.file1 = results.load_nd_from_pickle(filename='dingo_tests_grids_1.pkl')
+        self.file2 = results.load_nd_from_pickle(filename='dingo_tests_grids_2.pkl')
 
     def tearDown(self):
         print("cleanup")
 
-    def test_dingo_todataframe(self):
+    def test_equal(self):
+        equals, msg = self.dingo_equal(self.file1,self.file1)
+        self.assertTrue(equals,msg=msg)
+    def test_different(self):
+        equals, msg = self.dingo_equal(self.file1,self.file2)
+        self.assertFalse(equals,msg=msg)
 
+    def dingo_equal(self, file_1, file_2):
         #initiate dataframes through to_dataframe method
-        nodes_sav_df, edges_sav_df = self.nd_sav.to_dataframe()
-        nodes_new_df, edges_new_df = self.nd_new.to_dataframe()
+        nodes_one_df, edges_one_df = file_1.to_dataframe()
+        nodes_two_df, edges_two_df = file_2.to_dataframe()
+        #future: take the to_dataframe functions out of this function
+        #First, check if sizes of both are the same
+        if nodes_one_df.size!=nodes_two_df.size:
+            msg = 'Different number of nodes!'
+            return False, msg
+        elif edges_one_df.size!=edges_two_df.size:
+            msg = 'Different number of edges!'
+            return False, msg
+
+        #Second, check if IDs of both are the same
+        if not nodes_one_df['node_id'].equals(nodes_two_df['node_id']):
+            msg = 'Same number of nodes with different IDs!'
+            return False, msg
+        elif not edges_one_df['branch_id'].equals(edges_two_df['branch_id']):
+            msg = 'Same number of branches with different IDs!'
+            return False, msg
 
         #workaround because 'geom' is strange
         #    shapy geo information is in format WKB, which also includes somehow
         #    the memory slot where the data is stored. So we need to rescue just
         #    the data.
-        nodes_sav_df['geom'] = nodes_sav_df['geom'].apply(lambda x: x.desc)
-        nodes_new_df['geom'] = nodes_new_df['geom'].apply(lambda x: x.desc)
+        #For the Nodes:
+        nodes_one_df['geom'] = nodes_one_df['geom'].apply(lambda x: x.desc)
+        nodes_two_df['geom'] = nodes_two_df['geom'].apply(lambda x: x.desc)
 
-        edges_sav_df['geom'] = edges_sav_df['geom'].apply(lambda x: x.desc)
-        edges_new_df['geom'] = edges_new_df['geom'].apply(lambda x: x.desc)
-
-        #Prepare dataframe / workaround
-        #    The dataframes after to_dataframe do not necessarily have the same
-        #    index. That is, the element with 'node_id'='ID_1' with index = 0
-        #    in nodes_sav_df, does not necessarily have index = 0 in nodes_new_df
-        nodes_sav_df.sort_values('node_id', axis = 0, kind= 'mergesort', inplace=True)
-        nodes_new_df.sort_values('node_id', axis = 0, kind= 'mergesort', inplace=True)
-        nodes_sav_df = nodes_sav_df.reset_index(drop=True)
-        nodes_new_df = nodes_new_df.reset_index(drop=True)
-
-        edges_sav_df.sort_values('branch_id', axis = 0, kind= 'mergesort', inplace=True)
-        edges_new_df.sort_values('branch_id', axis = 0, kind= 'mergesort', inplace=True)
-        edges_sav_df = edges_sav_df.reset_index(drop=True)
-        edges_new_df = edges_new_df.reset_index(drop=True)
+        #Similar for edges, but the extreme nodes of an edge can be switched
+        #    first, convert to shape and resque coordinates
+        edges_one_geom_xy    = edges_one_df['geom'].apply(lambda x: to_shape(x)).apply(lambda x: x.xy)
+        edges_two_geom_xy    = edges_two_df['geom'].apply(lambda x: to_shape(x)).apply(lambda x: x.xy)
+        #    rescue data in the dataframe
+        edges_one_df['geom'] = edges_one_df['geom'].apply(lambda x: x.desc)
+        edges_two_df['geom'] = edges_two_df['geom'].apply(lambda x: x.desc)
+        #    then go through the edges and compare crossed coordinates
+        for idx, row in edges_one_geom_xy.iteritems():
+            if (    (edges_one_geom_xy[idx][0][0] == edges_two_geom_xy[idx][0][1])
+                and (edges_one_geom_xy[idx][0][1] == edges_two_geom_xy[idx][0][0])
+                and (edges_one_geom_xy[idx][1][0] == edges_two_geom_xy[idx][1][1])
+                and (edges_one_geom_xy[idx][1][1] == edges_two_geom_xy[idx][1][0])
+            ):
+                #if nodes are inverted, force both to be equal
+                edges_two_df.loc[idx,'geom'] = edges_one_df.loc[idx,'geom']
 
         # compare things
-        flag_nodes = nodes_sav_df.equals(nodes_new_df)
-        flag_edges = edges_sav_df.equals(edges_new_df)
+        flag_nodes = nodes_one_df.equals(nodes_two_df)
+        flag_edges = edges_one_df.equals(edges_two_df)
         passed     = flag_nodes and flag_edges
 
-        #do stuff in fail case
-        if not passed:
-            print('did not work')
-            edges_new_matrix = edges_new_df.as_matrix();
-            edges_sav_matrix = edges_sav_df.as_matrix();
-            #print(np.array_equal(edges_new_matrix, edges_sav_matrix))
-
-            permiso = True
-            for i in range(0, edges_new_matrix.size - 1):
-                if not np.array_equal(edges_new_matrix.item(i),
-                                      edges_sav_matrix.item(i)):
-                    #print(i, edges_new_matrix.item(i), 'NEQ',
-                    #      edges_sav_matrix.item(i))
-                    if permiso:
-                        print(edges_new_matrix[0])
-                        print(edges_sav_matrix[0])
-                        print(edges_new_matrix.item(i))
-                        permiso = False
-        else:
-            print('It worked!')
-
         #return result of test
-        self.assertTrue(passed)
+        msg = 'Data sets are'
+        if passed:
+            msg = msg + 'identical.'
+        else:
+            msg = msg + 'different.'
+        return passed, msg
 
-        # node_cols = ['node_id', 'grid_id', 'v_nom', 'geom', 'v_res0', 'v_res1',
-        #             'peak_load', 'generation_capacity', 'type']
-        # for index, row in nodes_sav_df.iterrows():
-        #    for col in node_cols:
-        #        print(index, col, nodes_new_df.iloc[index].T.loc(col))
-        # print(nodes_new_df.iloc[index])
-        # print(nodes_sav_df.iloc[index])
-        # print(nodes_sav_df.axes)
-        # index = 1;
-        # print(index, nodes_new_df.iloc[index])
-        # print(index, nodes_sav_df.iloc[index])
-
-def init_file_for_tests():
+def init_files_for_tests():
     # database connection
     conn = db.connection(section='oedb')
 
@@ -149,12 +108,23 @@ def init_file_for_tests():
     nd.export_mv_grid(conn, mv_grid_districts)
     nd.export_mv_grid_new(conn, mv_grid_districts)
 
-    conn.close()
+    # export grid to file (pickle)
+    save_nd_to_pickle(nd, filename='dingo_tests_grids_1.pkl')
+
+    # instantiate new dingo network object
+    nd = NetworkDingo(name='network')
+
+    # choose MV Grid Districts to import
+    mv_grid_districts = [428]
+
+    # run DINGO on selected MV Grid District
+    nd.run_dingo(conn=conn,mv_grid_districts_no=mv_grid_districts)
 
     # export grid to file (pickle)
-    save_nd_to_pickle(nd, filename='dingo_grids_example.pkl')
+    save_nd_to_pickle(nd, filename='dingo_tests_grids_2.pkl')
 
+    conn.close()
 
 if __name__ == "__main__":
     support.run_unittest(DingoRunTest)
-    #init_file_for_tests()
+    #init_files_for_tests()
