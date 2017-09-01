0428193098d9dcf6e374bbe424c81e362d48067a
nesnoj <jonathan.amme@rl-institut.de>
2017-09-01 17:55:21 +0200

    adjust MVGD id to get same MVGD as before (old id=3545)

--- ding0/examples/example_single_grid_district.py
+++ ding0/examples/example_single_grid_district.py
@@ -38,7 +38,8 @@ conn = db.connection(section='oedb')
 nd = NetworkDing0(name='network')
 
 # choose MV Grid Districts to import
-mv_grid_districts = [3545]
+#mv_grid_districts = [3545]
+mv_grid_districts = [134]
 
 # run DING0 on selected MV Grid District
 nd.run_ding0(conn=conn,
