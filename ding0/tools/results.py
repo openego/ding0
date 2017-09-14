3288b8d05de2e3596014f1ce1c82a42ea03a2202
nesnoj <jonathan.amme@rl-institut.de>
2017-09-14 18:48:06 +0200

    fix bug if cpu core count exceeds 2*MVGD count

--- ding0/tools/results.py
+++ ding0/tools/results.py
@@ -1269,6 +1269,9 @@ def parallel_running_stats(districts_list,
     max_dist = len(districts_list)
     threat_long = floor(max_dist / n_of_processes)
 
+    if threat_long == 0:
+        threat_long = 1
+
     threats = [districts_list[x:x + threat_long] for x in
                range(0, len(districts_list), threat_long)]
 
