fa043e1dc73c5e47930108ca128f7fa4d114abc7
Boltbeard <Bharadwaj.Narasimhan@rl-institut.de>
2019-01-17 14:46:01 +0100

    Creating Ding0 Tests in folder

---
+++ tests/core/test_core.py
@@ -0,0 +1,40 @@
+import pytest
+
+from egoio.tools import db
+from sqlalchemy.orm import sessionmaker
+import oedialect
+
+from ding0.core import NetworkDing0
+
+
+class TestNetworkDing0(object):
+
+    @pytest.fixture
+    def emptyNetworkDing0(self):
+        """
+        Returns an empty NetworkDing0 object for testing
+        """
+        return NetworkDing0()
+
+    @pytest.fixture
+    def oedb_session(self):
+        engine = db.connection(section='oedb')
+        session = sessionmaker(bind=engine)()
+        yield session
+        print("closing session")
+        session.close()
+
+    def test_empty_mv_grid_districts(emptyNetworkDing0):
+        mv_grid_districts = list(emptyNetworkDing0.mv_grid_districts())
+        empty_list = []
+        assert mv_grid_districts == empty_list
+
+    def test_import_mv_grid_districts(oedb_session):
+        with pytest.raises(TypeError):
+            NetworkDing0.import_mv_grid_districts(
+                oedb_session,
+                mv_grid_districts_no=['5']
+            )
+
+    # def test_run_ding0(self):
+    #     pass
---
+++ tests/core/test_network.py
@@ -0,0 +1,23 @@
+import pytest
+from ding0.core.network import GridDing0, \
+    GeneratorDing0, GeneratorFluctuatingDing0
+
+
+class TestGridDing0:(object):
+
+    @pytest.fixture
+    def empty_gridding0():
+        """
+        Returns an empty GridDing0 object
+        """
+        return GridDing0()
+
+    def test_mv_grid_districts(self):
+        pass
+
+    def test_run_ding0(self):
+        pass
+
+
+if __name__ == "__main__":
+    pass
