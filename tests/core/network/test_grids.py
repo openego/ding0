d9b8b2a36cf50c667ee52c636bc634e2ef453dd7
Boltbeard <Bharadwaj.Narasimhan@rl-institut.de>
2019-01-24 19:29:25 +0100

    Wrote a complete test outline for TestMVGridDing0 testing nx methods

--- tests/core/network/test_grids.py
+++ tests/core/network/test_grids.py
@@ -1,22 +1,223 @@
 import pytest
-from ding0.core.network import GridDing0, \
-    GeneratorDing0, GeneratorFluctuatingDing0
+from shapely.geometry import Point, LineString, LinearRing, Polygon
+from ding0.core import NetworkDing0
+from ding0.core.network import (RingDing0, BranchDing0, CircuitBreakerDing0,
+                                GeneratorDing0, GeneratorFluctuatingDing0)
+from ding0.core.network.stations import MVStationDing0, LVStationDing0
+from ding0.core.network.grids import MVGridDing0, LVGridDing0
 
 
-class TestGridDing0(object):
+class TestMVGridDing0(object):
 
     @pytest.fixture
-    def empty_gridding0(self):
+    def empty_mvgridding0(self):
         """
-        Returns an empty GridDing0 object
+        Returns an empty MVGridDing0 object
         """
-        return GridDing0()
+        station = MVStationDing0(id_db=0, geo_data=Point(0.5, 0.5))
+        grid = MVGridDing0(id_db=0,
+                           station=station)
+        return grid
 
-    def test_mv_grid_districts(self):
+    def test_empty_mvgridding0(self, empty_mvgridding0):
+        assert empty_mvgridding0._rings == []
+        assert empty_mvgridding0._circuit_breakers == []
+        assert empty_mvgridding0.default_branch_kind is None
+        assert empty_mvgridding0.default_branch_type is None
+        assert empty_mvgridding0.default_branch_kind_settle is None
+        assert empty_mvgridding0.default_branch_type_settle is None
+        assert empty_mvgridding0.default_branch_kind_aggregated is None
+        assert empty_mvgridding0.default_branch_type_aggregated is None
+        assert empty_mvgridding0._station.id_db == 0
+        assert empty_mvgridding0._station.geo_data == Point(0.5, 0.5)
+
+    def test_add_circuit_breakers(self, empty_mvgridding0):
+        circuit_breaker = CircuitBreakerDing0(id_db=0,
+                                              geo_data=Point(0, 0),
+                                              grid=empty_mvgridding0)
+        empty_mvgridding0.add_circuit_breaker(circuit_breaker)
+        circuit_breakers_in_grid = list(empty_mvgridding0.circuit_breakers())
+        assert len(circuit_breakers_in_grid) == 1
+        assert circuit_breakers_in_grid[0] == circuit_breaker
+
+    def test_add_circuit_breakers_negative(self, empty_mvgridding0):
+        bad_object = GeneratorDing0(id_db=0)
+        empty_mvgridding0.add_circuit_breaker(bad_object)
+        circuit_breakers_in_grid = list(empty_mvgridding0.circuit_breakers())
+        assert len(circuit_breakers_in_grid) == 0
+
+    @pytest.fixture
+    def circuit_breaker_mvgridding0(self):
+        """
+        Returns an MVGridDing0 object with a branch and a
+        circuit breaker
+        """
+        station = MVStationDing0(id_db=0, geo_data=Point(0.5, 0.5))
+        grid = MVGridDing0(id_db=0,
+                           station=station)
+        branch = BranchDing0(id_db=0, length=2.0, kind='cable')
+        circuit_breaker = CircuitBreakerDing0(id_db=0,
+                                              geo_data=Point(0, 0),
+                                              branch=branch,
+                                              grid=grid)
+        grid.add_circuit_breaker(circuit_breaker)
+        grid._graph.add_edge(circuit_breaker, station,
+                             branch=branch)
+        return grid
+
+    def test_open_circuit_breakers(self, circuit_breaker_mvgridding0):
+        circuit_breakers_in_grid = list(
+            circuit_breaker_mvgridding0.circuit_breakers()
+        )
+        assert circuit_breakers_in_grid[0].status == 'closed'
+        circuit_breaker_mvgridding0.open_circuit_breakers()
+        assert circuit_breakers_in_grid[0].status == 'open'
+
+    def test_close_circuit_breakers(self, circuit_breaker_mvgridding0):
+        circuit_breakers_in_grid = list(
+            circuit_breaker_mvgridding0.circuit_breakers()
+        )
+        assert circuit_breakers_in_grid[0].status == 'closed'
+        circuit_breaker_mvgridding0.open_circuit_breakers()
+        assert circuit_breakers_in_grid[0].status == 'open'
+        circuit_breaker_mvgridding0.close_circuit_breakers()
+        assert circuit_breakers_in_grid[0].status == 'closed'
+
+    @pytest.fixture
+    def ring_mvgridding0(self):
+        """
+        Returns an MVGridDing0 object with 2 branches
+        a circuitbreaker and a ring
+        """
+        station = MVStationDing0(id_db=0, geo_data=Point(1, 1))
+        grid = MVGridDing0(id_db=0,
+                           station=station)
+        generator1 = GeneratorDing0(id_db=0,
+                                    geo_data=Point(1, 2),
+                                    mv_grid=grid)
+        grid.add_generator(generator1)
+        generator2 = GeneratorDing0(id_db=1,
+                                    geo_data=Point(2, 1),
+                                    mv_grid=grid)
+        grid.add_generator(generator2)
+        generator3 = GeneratorDing0(id_db=2,
+                                    geo_data=Point(2, 2),
+                                    mv_grid=grid)
+        grid.add_generator(generator3)
+        ring = RingDing0(grid=grid)
+        branch1 = BranchDing0(id_db='0', length=2.0, kind='cable', ring=ring)
+        branch1a = BranchDing0(id_db='0a', lenght=1.2, kind='cable', ring=ring)
+        branch2 = BranchDing0(id_db='1', lenght=3.0, kind='line', ring=ring)
+        branch2a = BranchDing0(id_db='1a', lenght=2.0, kind='line', ring=ring)
+        branch3 = BranchDing0(id_db='2', length=2.5, kind='line')
+        circuit_breaker1 = CircuitBreakerDing0(id_db=0,
+                                               geo_data=Point(0, 0),
+                                               branch=branch1,
+                                               grid=grid)
+        grid.add_circuit_breaker(circuit_breaker1)
+        grid._graph.add_edge(generator1, station,
+                             branch=branch1)
+        grid._graph.add_edge(circuit_breaker1, generator1,
+                             branch=branch1a)
+        grid._graph.add_edge(generator2, station,
+                             branch=branch2)
+        grid._graph.add_edge(circuit_breaker1, generator2,
+                             branch=branch2a)
+        grid._graph.add_edge(generator3, generator2, branch=branch3)
+        grid.add_ring(ring)
+        return (ring, grid)
+
+    def test_add_ring(self, ring_mvgridding0):
+        ring, grid = ring_mvgridding0
+        assert len(grid._rings) == 1
+        assert grid._rings[0] == ring
+
+    def test_rings_count(self, ring_mvgridding0):
+        ring, grid = ring_mvgridding0
+        assert grid.rings_count() == 1
+        assert grid._rings[0] == ring
+
+    def test_get_ring_from_node(self, ring_mvgridding0):
+        ring, grid = ring_mvgridding0
+        station = grid.station()
+        assert grid.get_ring_from_node(station) == ring
+
+    def test_rings_nodes_root_only_include_root(self, ring_mvgridding0):
+        ring, grid = ring_mvgridding0
+        station = grid.station()
+        generators = list(grid.generators())
+        circuit_breakers = list(grid.circuit_breakers())
+        rings_nodes_expected = [generators[0],
+                                circuit_breakers[0],
+                                generators[1],
+                                station]
+        rings_nodes = list(grid.rings_nodes(include_root_node=True))[0]
+        assert rings_nodes == rings_nodes_expected
+
+    def test_rings_nodes_root_only_exclude_root(self, ring_mvgridding0):
+        ring, grid = ring_mvgridding0
+        generators = list(grid.generators())
+        circuit_breakers = list(grid.circuit_breakers())
+        rings_nodes_expected = [generators[0],
+                                circuit_breakers[0],
+                                generators[1]]
+        rings_nodes = list(grid.rings_nodes(include_root_node=False))[0]
+        assert rings_nodes == rings_nodes_expected
+
+    def test_rings_nodes_include_satellites_include_root(self,
+                                                         ring_mvgridding0):
+        ring, grid = ring_mvgridding0
+        station = grid.station()
+        generators = list(grid.generators())
+        circuit_breakers = list(grid.circuit_breakers())
+        rings_nodes_expected = [generators[0],
+                                circuit_breakers[0],
+                                generators[1],
+                                station,
+                                generators[2]]
+        rings_nodes = list(grid.rings_nodes(include_root_node=True,
+                                            include_satellites=True))[0]
+        assert rings_nodes == rings_nodes_expected
+
+    def test_rings_nodes_include_satellites_exclude_root(self,
+                                                         ring_mvgridding0):
+        ring, grid = ring_mvgridding0
+        generators = list(grid.generators())
+        circuit_breakers = list(grid.circuit_breakers())
+        rings_nodes_expected = [generators[0],
+                                circuit_breakers[0],
+                                generators[1],
+                                generators[2]]
+        rings_nodes = list(grid.rings_nodes(include_root_node=False,
+                                            include_satellites=True))[0]
+        assert rings_nodes == rings_nodes_expected
+
+    def test_rings_full_data(self, ring_mvgridding0):
         pass
 
-    def test_run_ding0(self):
+    def test_graph_nodes_from_subtree(self, ring_mvgridding0):
         pass
+
+    def test_set_branch_ids(self, ring_mvgridding0):
+        pass
+
+    def test_routing(self, ring_mvgridding0):
+        pass
+
+    def test_connect_generators(self, ring_mvgridding0):
+        pass
+
+
+class TestLVGridDing0(object):
+
+    @pytest.fixture
+    def empty_lvgridding0(self):
+        """
+        Returns and empty LVGridDing0 object
+        """
+        lv_station = LVStationDing0(id_db=0, geo_data=Point(1, 1))
+        grid = LVGridDing0(id_db=0, station=lv_station)
+        return grid
 
 
 if __name__ == "__main__":
