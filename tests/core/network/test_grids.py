f3aca2bc8b65eecfc9b804557f7737a8775dd047
Santiago <santiago.infantino.moreno@gmail.com>
2019-04-02 15:38:52 +0200

    Correction in build_ria_branches 2

--- tests/core/network/test_grids.py
+++ tests/core/network/test_grids.py
@@ -1512,7 +1512,7 @@ class TestLVGridDing0(object):
         #2 Branches from LV_station
         assert len(list(nx.all_neighbors(
             basic_lv_grid._graph,
-            nx.nodes(basic_lv_grid._graph[0]))) == 2)
+            list(basic_lv_grid._graph.node)[0]))) == 2)
 
     def test_connect_generators(self, basic_lv_grid):
         """
