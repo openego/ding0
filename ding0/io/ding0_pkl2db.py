5b3281a05da1f1cc60963d2ce962667dab393a92
Jonas H <jonas.huber@rl-institut.de>
2019-07-12 20:19:56 +0200

    renamed variable SRID to PICKLE_SRID

--- ding0/io/ding0_pkl2db.py
+++ ding0/io/ding0_pkl2db.py
@@ -1,39 +1,32 @@
 # coding: utf-8
+"""This file is part of DINGO, the DIstribution Network GeneratOr.
+DINGO is a tool to generate synthetic medium and low voltage power
+distribution grids based on open data.
+
+It is developed in the project open_eGo: https://openegoproject.wordpress.com
+
+DING0 lives at github: https://github.com/openego/ding0/
+The documentation is available on RTD: http://ding0.readthedocs.io"""
+
+__copyright__ = "Reiner Lemoine Institut gGmbH"
+__license__ = "GNU Affero General Public License Version 3 (AGPL-3.0)"
+__url__ = "https://github.com/openego/ding0/blob/master/LICENSE"
+__author__ = "jh-RLI"
 
 import os
-
-import numpy as np
-import json
-
 from egoio.tools import db
-
-# import required modules of DING0
-from ding0.tools.logger import setup_logger
+from ding0.io.exporter_log import pickle_export_logger
 from ding0.tools.results import load_nd_from_pickle
 from ding0.io.export import export_network
 from ding0.io.db_export import METADATA, create_ding0_sql_tables, \
     export_all_pkl_to_db, db_tables_change_owner, drop_ding0_db_tables
 from sqlalchemy.orm import sessionmaker
 
-# ToDo: Create logger as function
 ##################################
 # LOG_FILE_PATH = 'pickle_log'
 LOG_FILE_PATH = os.path.join(os.path.expanduser("~"), '.ding0_log', 'pickle_log')
-
-# does the file exist?
-if not os.path.isfile(LOG_FILE_PATH):
-    print('ding0 log-file {file} not found. '
-          'This might be the first run of the tool. '
-          .format(file=LOG_FILE_PATH))
-    base_path = os.path.split(LOG_FILE_PATH)[0]
-    if not os.path.isdir(base_path):
-        os.mkdir(base_path)
-        print('The directory {path} was created.'.format(path=base_path))
-
-    with open(LOG_FILE_PATH, 'a') as log:
-        log.write("List of missing grid districts:")
-        pass
-######################################################
+pickle_export_logger(LOG_FILE_PATH)
+###################################
 
 # database connection/ session
 oedb_engine = db.connection(section='oedb')
@@ -50,7 +43,7 @@ pkl_filepath = "/home/local/RL-INSTITUT/jonas.huber/rli/Daten_flexibel_01/Ding0/
 
 
 # choose MV Grid Districts to import use list of integers
-# f. e.: grids = list(range(1, 3609))
+# f. e.: multiple grids = list(range(1, 3609))
 grids = [1658]
 
 # generate all the grids and push them to oedb
@@ -67,13 +60,16 @@ for grid_no in grids:
         continue
 
     # Extract data from network and put it to DataFrames for csv and for oedb
+    # run_id is manually provided -> folder name or nw.metadata['run_id'] provide the run_id value
     network = export_network(nw, run_id=20180823154014)
 
-    # Send data to OEDB
-    srid = int(nw.config['geo']['srid'])
+    # set SRID form pickle file
+    PICKLE_SRID = int(nw.config['geo']['srid'])
+
 
     # provide run id for pickle upload
 
-    export_all_pkl_to_db(oedb_engine, SCHEMA, network, srid, grid_no)
+    export_all_pkl_to_db(oedb_engine, SCHEMA, network, PICKLE_SRID, grid_no)
+
 
 # db_tables_change_owner(oedb_engine, SCHEMA)
