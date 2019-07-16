f4f2e99c86ee7f9d4f7a8b013d3ec34ad05ecf59
Jonas H <jonas.huber@rl-institut.de>
2019-07-16 18:58:27 +0200

    add further information in docstring

--- ding0/io/exporter_log.py
+++ ding0/io/exporter_log.py
@@ -1,3 +1,17 @@
+"""This file is part of DINGO, the DIstribution Network GeneratOr.
+DINGO is a tool to generate synthetic medium and low voltage power
+distribution grids based on open data.
+
+It is developed in the project open_eGo: https://openegoproject.wordpress.com
+
+DING0 lives at github: https://github.com/openego/ding0/
+The documentation is available on RTD: http://ding0.readthedocs.io"""
+
+__copyright__ = "Reiner Lemoine Institut gGmbH"
+__license__ = "GNU Affero General Public License Version 3 (AGPL-3.0)"
+__url__ = "https://github.com/openego/ding0/blob/master/LICENSE"
+__author__ = "jh-RLI"
+
 import os
 
 # LOG_FILE_PATH = 'pickle_log'
@@ -7,6 +21,12 @@ def pickle_export_logger(log_file_path):
     """
     Creates a list for pickle files that are missing for some reason.
     Most likely the file does not exists @ the pickle file path dir.
+
+    Log missing ding0 GridDistricts:
+    The export_log provides functionality that set ups a logging file. One need to provide a path to the destination
+    the logfile shall be stored in.
+    The file can be opened at any code line and input can be provided. This logger is mainly used within a exception
+    to log the missing GridDistricts that could not be created by ding0.
 
     :param log_file_path:
     :return:
