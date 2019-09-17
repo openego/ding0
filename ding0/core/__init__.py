234a921e85d35c9fa0de999460624ba3a78c7d8f
AnyaHe <Anya.Heider@rl-institut.de>
2019-09-17 10:03:53 +0200

    Revert "changes concerning trafo data and consistant naming of electrical parameters"
    This reverts commit 72600b8f

--- ding0/core/__init__.py
+++ ding0/core/__init__.py
@@ -1175,7 +1175,7 @@ class NetworkDing0:
         static_data['MV_trafos'] = pd.read_csv(os.path.join(package_path, 'data',
                                    equipment_mv_parameters_trafos),
                                    comment='#',
-                                   delimiter=';',
+                                   delimiter=',',
                                    decimal='.',
                                    converters={'S_nom': lambda x: int(x)})
 
@@ -1185,8 +1185,6 @@ class NetworkDing0:
         static_data['MV_overhead_lines'] = pd.read_csv(os.path.join(package_path, 'data',
                                            equipment_mv_parameters_lines),
                                            comment='#',
-                                           delimiter=';',
-                                           decimal='.',
                                            converters={'I_max_th': lambda x: int(x),
                                                        'U_n': lambda x: int(x),
                                                        'reinforce_only': lambda x: int(x)})
@@ -1196,8 +1194,6 @@ class NetworkDing0:
         static_data['MV_cables'] = pd.read_csv(os.path.join(package_path, 'data',
                                    equipment_mv_parameters_cables),
                                    comment='#',
-                                   delimiter=';',
-                                   decimal='.',
                                    converters={'I_max_th': lambda x: int(x),
                                                'U_n': lambda x: int(x),
                                                'reinforce_only': lambda x: int(x)})
@@ -1207,8 +1203,6 @@ class NetworkDing0:
         static_data['LV_cables'] = pd.read_csv(os.path.join(package_path, 'data',
                                    equipment_lv_parameters_cables),
                                    comment='#',
-                                   delimiter=';',
-                                   decimal='.',
                                    index_col='name',
                                    converters={'I_max_th': lambda x: int(x), 'U_n': lambda x: int(x)})
 
@@ -1217,7 +1211,7 @@ class NetworkDing0:
         static_data['LV_trafos'] = pd.read_csv(os.path.join(package_path, 'data',
                                    equipment_lv_parameters_trafos),
                                    comment='#',
-                                   delimiter=';',
+                                   delimiter=',',
                                    decimal='.',
                                    converters={'S_nom': lambda x: int(x)})
         static_data['LV_trafos']['r_pu'] = static_data['LV_trafos']['P_k'] / (static_data['LV_trafos']['S_nom']*1000)
