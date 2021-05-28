72773814e16cd056bf91e3177c9b25ba604662b7
nesnoj <jonathan.amme@rl-institut.de>
2021-05-29 00:47:12 +0200

    bump version

--- doc/conf.py
+++ doc/conf.py
@@ -124,7 +124,7 @@ author = u'open_eGo-Team'
 # built documents.
 #
 # The short X.Y version.
-version = '0.2.0'
+version = '0.2.1'
 # The full version, including alpha/beta/rc tags.
 #release = '0.1.10'
 
