885b287b545b648dd879b8d4f131bae3b436192d
Malte Jahn <malte.jahn@rl-institut.de>
2022-10-11 08:45:22 +0200

    Remove unused duplicate function

--- ding0/config/db_conn_local.py
+++ ding0/config/db_conn_local.py
@@ -7,31 +7,6 @@ local loading from sandbox_bw will be deprecated.
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-
-def create_session_osm():
-    """
-    return Session() for sql queries
-    """
-
-
-'''
-    # sqlachemy local osm data
-    DB = 'postgresql'
-    DB_USER = 'postgres'
-    DB_PW   = 'labor'
-    DB_Name = 'sandbox_bw' # osm data baden-wuerttemberg
-
-
-    engine_osm = create_engine(DB + '://' + DB_USER + ': ' + DB_PW + '@localhost:5432/' + DB_Name, echo=False)
-
-    Session = sessionmaker(bind=engine_osm)
-
-    return Session()
-
-
-'''
-
-
 def create_session_osm():
     """SQLAlchemy session object with valid connection to sonnja database"""
 
