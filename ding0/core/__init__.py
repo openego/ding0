c042835b75d45e051eb81fe7738cacf9d113a3c6
Malte Jahn <malte.jahn@rl-institut.de>
2023-01-19 15:07:04 +0100

    linting

--- ding0/core/__init__.py
+++ ding0/core/__init__.py
@@ -2079,9 +2079,15 @@ class NetworkDing0:
         self.control_circuit_breakers(mode='open')
         # start filling component dataframes
         for grid_district in self.mv_grid_districts():
-            gd_components, network_df, _, _ = fill_mvgd_component_dataframes(grid_district, buses_df, generators_df,
-                                                                          lines_df, loads_df, transformer_df,
-                                                                          only_export_mv)
+            gd_components, network_df, _, _ = fill_mvgd_component_dataframes(
+                grid_district,
+                buses_df,
+                generators_df,
+                lines_df,
+                loads_df,
+                transformer_df,
+                only_export_mv
+            )
             if len(components) == 0:
                 components = gd_components
                 networks = network_df
